9c6131f51cff7d1ca424eed29d51dad5aa5eca71
harshneu <hrsh.shuklaa@gmail.com>
2017-02-15 19:38:46 -0500

    Revert "Revert "Tagged Entry""
    This reverts commit daa24a2658e857295915edafe1fbe4e0b316f934.

--- public/assignment/services/page.service.client.js
+++ public/assignment/services/page.service.client.js
@@ -6,9 +6,9 @@
     function PageService() {
         var pages = [
             {_id: "321", name : "Post 1", websiteId: "123", description:"Lorem"},
-            {_id: "432", name : "Post 2", websiteId: "111", description:"Lorem"},
-            {_id: "543", name : "Post 3", websiteId: "111", description:"Lorem"},
-            {_id: "123", name : "Post 4", websiteId: "123", description:"Lorem"},
+            {_id: "432", name : "Post 2", websiteId: "234", description:"Lorem"},
+            {_id: "543", name : "Post 3", websiteId: "234", description:"Lorem"},
+            {_id: "123", name : "Post 4", websiteId: "678", description:"Lorem"},
             {_id: "456", name : "Post 5", websiteId: "123", description:"Lorem"}
         ];
 
