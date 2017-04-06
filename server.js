50e64e7cd2b4f9f80598eb70162e6201d772071b
harshneu <hrsh.shuklaa@gmail.com>
2017-04-06 19:46:08 -0400

    Sever

--- server.js
+++ server.js
@@ -13,14 +13,25 @@ var mongoose = require('mongoose');
 var connectionString = 'mongodb://127.0.0.1:27017/formmaker';
 //var connectionString = 'mongodb://127.0.0.1:27017/prismaticmusic';
 
-//console.log(process.env);
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-}
+var express = require('express');
+var app = express();
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
+
+require ("./test/app.js")(app);
+
+// Our assignment server
+require("./assignment/app.js")(app);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
 
 var db = mongoose.connect(connectionString);
 
