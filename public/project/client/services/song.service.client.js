f32b847587a69d15f852325fa9dacabd76e73596
harshneu <hrsh.shuklaa@gmail.com>
2017-04-17 16:29:06 -0400

    Push commit

--- public/project/client/services/song.service.client.js
+++ public/project/client/services/song.service.client.js
@@ -3,7 +3,6 @@
     var TRACK_URL = "http://ws.audioscrobbler.com/2.0/?api_key=660fbf2c8a0c4658c2dd385ac4273bc2&format=json&method=song.search&song=TRACK&autocorrect=1";
     var INFO_URL = "http://ws.audioscrobbler.com/2.0/?api_key=660fbf2c8a0c4658c2dd385ac4273bc2&format=json&method=song.getInfo&mbid=MBID&autocorrect=1";
     var TOPTRACK_URL = "http://ws.audioscrobbler.com/2.0/?api_key=660fbf2c8a0c4658c2dd385ac4273bc2&format=json&method=artist.getTopTracks&mbid=MBID&autocorrect=1";
-    var SIMILAR_URL = "http://ws.audioscrobbler.com/2.0/?api_key=660fbf2c8a0c4658c2dd385ac4273bc2&format=json&method=song.getsimilar&mbid=MBID&autocorrect=1";
     var TRACKARTIST_URL = "http://ws.audioscrobbler.com/2.0/?api_key=660fbf2c8a0c4658c2dd385ac4273bc2&format=json&method=song.getInfo&song=TRACK&artist=ARTIST&autocorrect=1";
 
     angular
