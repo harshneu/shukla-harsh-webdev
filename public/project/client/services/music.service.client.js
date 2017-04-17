4b839918ac0c8c6218edd17e9516b0f559c519f0
harshneu <hrsh.shuklaa@gmail.com>
2017-04-17 16:57:15 -0400

    Revert "Deleted favorite user functionality"
    This reverts commit bd796a0f2889494e739c17f620a5fcbca52a5dae.

--- public/project/client/services/music.service.client.js
+++ public/project/client/services/music.service.client.js
@@ -10,6 +10,9 @@
             findAllComments: findAllComments,
             postComment: postComment,
             deleteComment: deleteComment,
+            findFavoriteUsers: findFavoriteUsers,
+            postFavoriteUser: postFavoriteUser,
+            removeFavoriteUser: removeFavoriteUser
         };
         return api;
 
@@ -25,5 +28,16 @@
             return $http.delete('/api/project/music/' + mbId + '/comment/' + commentId);
         }
 
+        function findFavoriteUsers(mbId) {
+            return $http.get('/api/project/music/' + mbId + '/user');
+        }
+
+        function postFavoriteUser(userId, username, music) {
+            return $http.post("/api/project/user/" + userId + "/" + username + "/music/" + music.mbId, music);
+        }
+
+        function removeFavoriteUser(userId, mbId) {
+            return $http.delete("/api/project/music/" + mbId + "/user/" + userId);
+        }
     }
 })();
